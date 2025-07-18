7898fa553eb21214290e8d74080ef9a9509f3b9d
Abdou <hipexx32@gmail.com>
2025-07-18 03:19:19 +0100

    Updating Routes

--- src/main/java/com/rentgo/gateway/security/SecurityConfig.java
+++ src/main/java/com/rentgo/gateway/security/SecurityConfig.java
@@ -18,8 +18,8 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/actuator/**").permitAll()
-                        .anyExchange().permitAll()
+                        .pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
